5b627530ae355a9f88285b572e100ed6c3289606
Muskaan <muskaan19010@gmail.com>
2022-11-11 13:16:54 +0530

    merge sort & quick sort

--- array-manipulation/sorting.js
+++ array-manipulation/sorting.js
@@ -26,15 +26,94 @@ const insertionSort = (unsortedArr) => {
   }
   console.log(unsortedArr);
 };
-insertionSort(unsortedArr);
+const sortUsingInsertion = unsortedArr;
+insertionSort(sortUsingInsertion);
 
 // Q2. Merge sort - O(n logn)
+
+function solve(arr, start, mid, end) {
+  const arr1 = arr.slice(start, mid + 1);
+  const arr2 = arr.slice(mid + 1, end + 1);
+  // console.log(arr1, arr2);
+  let i = 0,
+    j = 0,
+    k = start;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) arr[k++] = arr1[i++];
+    else arr[k++] = arr2[j++];
+  }
+
+  while (i < arr1.length) {
+    arr[k++] = arr1[i++];
+  }
+  while (j < arr2.length) {
+    arr[k++] = arr2[j++];
+  }
+}
+
+function mergeSort(arr, start, end) {
+  if (start >= end) return; // base case
+
+  const mid = start + Math.floor((end - start) / 2);
+
+  mergeSort(arr, start, mid);
+  mergeSort(arr, mid + 1, end);
+
+  solve(arr, start, mid, end);
+}
+
+const sortUsingMergeAlgo = unsortedArr;
+mergeSort(sortUsingMergeAlgo, 0, sortUsingMergeAlgo.length - 1);
+console.log(sortUsingMergeAlgo);
 
 // Q3. Bubble sort
 
 // Q4. Heap sort
 
 // Q5. Quick sort
+
+const swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+const partition = (arr, start, end) => {
+  let count = 0;
+  let pivot = arr[start];
+
+  for (let i = start; i <= end; i++) {
+    if (arr[i] < pivot) count++;
+  }
+  const pivotIndex = start + count;
+  swap(arr, start, pivotIndex);
+
+  let i = start,
+    j = end;
+  while (i < pivotIndex && j > pivotIndex) {
+    while (arr[i] <= pivot) i++;
+    while (arr[j] > pivot) j--;
+
+    if (i < pivotIndex && j > pivotIndex) {
+      swap(arr, i, j);
+      i++;
+      j--;
+    }
+  }
+  return pivotIndex;
+};
+
+const quickSort = (arr, start, end) => {
+  const partition = partition(arr, start, end);
+
+  quickSort(arr, start, partition);
+  quickSort(arr, partition + 1, end);
+};
+
+const sortUsingQuickAlgo = unsortedArr;
+quickSort(sortUsingQuickAlgo, 0, sortUsingQuickAlgo.length - 1);
+console.log(sortUsingQuickAlgo);
 
 // Q6. Selection sort
 
