4cae26b97e63123f945a86847d4211e8e504eeb4
Muskaan <muskaan19010@gmail.com>
2022-12-03 19:33:46 +0530

    Two um problem with O(n) & O(n^2) approach

---
+++ array-manipulation/twoSum.js
@@ -0,0 +1,32 @@
+// Q. Two sum - Given an array A and an integer target, find the indices of the two numbers in the array whose sum is equal to the given target.
+
+function twoSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const diffIndex = array.indexOf(target - array[i]); // has value -1 if no element found
+    if (diffIndex >= 0 && diffIndex !== i) {
+      return [i, diffIndex];
+    }
+  }
+  return []; // no solution found
+}
+
+const ans = twoSum([1, 5, 4, 8, 9], 13);
+console.log(ans); // [1,3]
+
+const twoSumUsingHash = (array, target) => {
+  const hashtable = {};
+  for (let i = 0; i < array.length; i++) {
+    hashtable[array[i]] = i;
+  }
+  for (let j = 0; j < array.length; j++) {
+    let complement = target - array[j];
+    // The second condition checks for cases like twoSum([7,7], 14) where the indices of similar elements MUST be different
+    if (hashtable.hasOwnProperty(complement) && hashtable[complement] !== j) {
+      return [hashtable[complement], j];
+    }
+  }
+  return null;
+};
+
+const output = twoSumUsingHash([9, 9], 18);
+console.log(output); // [1,0]
